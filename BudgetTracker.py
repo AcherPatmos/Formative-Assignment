6eedd72ec78fff1d9fbf334cbf7cd37fb0990aeb
Patmos <a.mpakaniye@alustudent.com>
2025-11-27 08:58:32 +0200

    created separate files for the different class objects I wrote

--- BudgetTracker.py
+++ BudgetTracker.py
@@ -1,30 +1,12 @@
-
-# I imported the datetime library because the project will involve time manipulation
-
 from datetime import datetime, timedelta
+from tabulate import tabulate
+from Transactions import Income, Expense
 
-#class definition
 class BudgetTracker:
-# initializing the transaction list to be accessed by other self-functions
     def __init__(self):
         self.Transactions=[]
-    def greet_user(self):
-        print("Welcome to BudgetTracker!\n")
-        print("welcome to your BudgetTracker\n")
-        name = input("please enter your name: \n").upper()
-        print("Hello" + " " + name + " " + "let's get started with your budget tracking!\n")
-        return name
-# I added in a menu section that would allow users to have options to choose from
-    def menu(self):
-        print("choose one of the options below")
-        print("1. add transactions")
-        print("2. list transactions")
-        print("3. summarize budget")
-        print("4. filter transactions")
-        print("5. exit")
-# I created the validate_date function so that only valid date inputs can be used for the date of transaction.
-# Otherwise, it loops and prompts the user to put in the correct date. It is also limited to the current year
-# we are in which is 2025
+        self.balance=0
+
     def validate_date(self):
         while True:
             date=input("Enter the date of the transaction (dd/mm/yyyy): \n")
@@ -40,100 +22,131 @@ class BudgetTracker:
             except ValueError:
                 print("Please enter a valid date and year \n")
 
-# I defined the add_transactions function so that it can give the user options to add in their transaction
-    def add_transactions(self):
-        t_date = self.validate_date()
+    def get_amount(self):
         # I used the while true loop to make sure the user would always be prompted to write a valid amount if they put in a negative number
         # Currencies are also not supported. The try is to handle invalid inputs gracefully instead of python crashing
         while True:
-            try:
-                amount = (input("enter amount of transaction: "))
-                if int(amount) < 0:
-                    print("Invalid amount!! Please input a positive amount \n")
-                    continue
-                break
-            except ValueError:
-                print("please enter a valid amount \n")
-        transaction_type = input("enter type of transaction(income/expense): ")
-        description = input("enter description of transaction: ")
-        transaction = {
-            "date": t_date,
-            "amount": amount,
-            "description": description,
-            "transaction_type": transaction_type
-        }
-        self.Transactions.append(transaction)
-        print("transaction added successfully\n")
-# this is the list transaction function which access the Transaction list at the beginning and the user is able
-# to see all the transactions they recorded in order through using enumerate.
+                try:
+                    amount = float(input("Amount: $"))
+                    if amount < 0:
+                        print("Amount cannot be negative! please enter a valid amount")
+                        continue
+                    return amount
+                except ValueError:
+                    print("Please enter a valid number.")
+
+    def add_income(self):
+        t_date = self.validate_date()
+        amount= self.get_amount()
+        description = input("enter transaction category(eg:Salary,Groceries): ")
+        inc=Income(t_date,amount,description)
+        self.Transactions.append(inc)
+        self.balance+=amount
+        print(f"transaction recorded successfully Current balance= ${self.balance}")
+
+    def add_expense(self):
+        t_date = self.validate_date()
+        amount = self.get_amount()
+        description = input("enter transaction category(eg:Salary,Groceries): ")
+        exp=Expense(t_date,amount,description)
+        self.Transactions.append(exp)
+        self.balance-=amount
+        print(f"transaction recorded successfully Current balance= ${self.balance}")
+
     def list_transactions(self):
         if not self.Transactions:
-            print("No transactions recorded yet.\n")
+            print("No transactions recorded yet.")
             return
-        for y, x in enumerate(self.Transactions, start=1):
-            print(f"transaction {y}: {x['date']} | {x['amount']} | {x['transaction_type']} | {x['description']}\n")
+        table=[]
+        for x in self.Transactions:
+            table.append([x.date,x.t_type.upper(),x.amount,x.description])
+        headers=["Date","Type","Amount","Description"]
+        print("Transactions list")
+        print(tabulate(table,headers,tablefmt="fancy_grid"))
+        print(f'current balance= ${self.balance}')
 
     def filter_transactions(self):
         print("choose one of the filter options below: ")
-        print("1. filter by date")
-        print("2. filter by transaction type")
+        print("1. filter by Date")
+        print("2. filter by Transaction type")
         print("3. filter by description")
-        print("4. filter by amount")
-        option=int(input("enter your choice: "))
-        if option==1:
-            while True:
-                month_input = input("enter month of transaction you want to filter(yyyy-mm): ")
-                results = []
-                for x in self.Transactions:
-                    date = x['date'].strftime("%Y-%m")
-                try:
-                    if date==month_input:
-                        results.append(x)
-                    if not results:
-                        print("please enter a valid month")
-                        recorded_months=sorted({x['date'].strftime("%Y-%m") for x in self.Transactions})
-                        print("these are the recorded months to filter:" ," ," .join(recorded_months),"\n")
-                        continue
-                    return results
-                except ValueError:
-                    print("please enter a valid month")
-        elif option==2:
-            while True:
-                transaction_type_input = input("enter transaction type(income/expense): ").lower()
-                if transaction_type_input=="income" not in ["income", "expense"]:
-                    print("please enter a valid transaction type")
-                    continue
-                results=[]
-                for x in self.Transactions:
-                    if x['transaction_type'].lower()==transaction_type_input:
-                        results.append(x)
-                    if not results:
-                        print("please enter a valid transaction type")
-                    else:
-                        return results
+        print("4. exit")
+        while True:
+            option= int(input("Enter your choice: "))
+            try:
+                if option == 1:
+                    self.filter_by_month()
+                elif option == 2:
+                    self.filter_by_type()
+                elif option == 3:
+                    self.filter_by_description()
+                elif option == 4:
+                    print("returning to main menu")
+                    return
+                else:
+                    print("Invalid choice. Try again.")
+            except ValueError:
+                print("please enter a valid choice")
 
-# this is the function that runs the whole program and the while true loop helps make sure the user is always given
-# the menu option after completing a process
+    def display_results(self, results):
+        if not results:
+            print("No matching transactions found")
+            return
+        table = [
+            [x.date.strftime("%Y/%m"), x.t_type.upper(), x.amount, x.description]
+            for x in results
+        ]
+        print("\nFiltered Results:")
+        print(tabulate(table, ["Date", "Type", "Amount", "Description"], tablefmt="fancy_grid"))
 
-Main_program=BudgetTracker()
+    def filter_by_month(self):
+        if not self.Transactions:
+            print("\nNo transactions recorded yet.\n")
+            return
+        recorded_months = sorted({t.date.strftime("%Y-%m") for t in self.Transactions})
+        print("\nRecorded months to filter:", ", ".join(recorded_months))
+        while True:
+            dt = input("Enter a date (YYYY/MM): ")
+            if len(dt) != 7 or dt[4] != "/":
+                print("Invalid format! Use YYYY/MM (e.g., 2025/04).")
+                continue
+            results = [t for t in self.Transactions if t.date.strftime("%Y/%m") ==dt]
+            if results:
+                self.display_results(results)
+                return
+            print("Invalid month or no transactions in that month.")
 
-while True:
-    Main_program.menu()
-    choice=int(input("enter your choice: "))
-    try:
-        if choice==1:
-            Main_program.add_transactions()
-        elif choice==2:
-            Main_program.list_transactions()
-        elif choice==4:
-            Main_program.filter_transactions()
-        else:
-            print("please enter a valid choice\n")
-    except ValueError:
-        print("please enter a valid choice\n")
+    def filter_by_type(self):
+        if not self.Transactions:
+            print("\nNo transactions recorded yet.\n")
+            return
+        recorded_types = sorted({t.t_type for t in self.Transactions})
+        print("\nAvailable transaction types:", ", ".join(t.lower() for t in recorded_types))
+        while True:
+            transaction_type = input("Enter a transaction type(income/expense): ")
+            if transaction_type not in ["income", "expense"]:
+                print("\nInvalid type! Must be 'income' or 'expense'.")
+                continue
+            if transaction_type not in recorded_types:
+                print(f"\nNo '{transaction_type}'transactions recorded yet.")
+                continue
+            result=[t for t in self.Transactions if t.t_type == transaction_type]
+            if result:
+                self.display_results(result)
+                return
 
-
-
-
-
-
+    def filter_by_description(self):
+        if not self.Transactions:
+            print("\nNo transactions recorded yet.\n")
+            return
+        recorded_descriptions = sorted({t.description for t in self.Transactions})
+        print("\nRecorded descriptions available:", ", ".join(recorded_descriptions))
+        while True:
+            keyword = input("Enter keyword to search in descriptions: ").strip().lower()
+            results = [
+                t for t in self.Transactions
+                if keyword in t.description.lower()
+            ]
+            if results:
+                self.display_results(results)
+                return
